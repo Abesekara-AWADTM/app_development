ed2d2091414e8a2d6123d595213868410e3e4468
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-09-05 23:00:59 +0530

    update

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,18 +9,6 @@ function Expenses(props) {
   // console.log(props);
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  // let filterInfoText = "2019, 2021 & 2022";
-
-  // if (filteredYear === "2019") {
-  //   filterInfoText = "2020,2021 & 2022";
-  // } else if (filteredYear === "2020") {
-  //   filterInfoText = "2019,2021 & 2022";
-  // } else if (filteredYear === "2021") {
-  //   filterInfoText = "2019,2020 & 2022";
-  // } else {
-  //   filterInfoText = "2019,2020 & 2021";
-  // }
-
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
